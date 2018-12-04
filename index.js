e86954f480e8e817dadd6a892d28e53b84e5b3f7
Leonardo Peralta <leo.pm15@gmail.com>
2018-12-04 04:49:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,19 +9,15 @@ return instrumentsPlayedBy;
 }
 
 function johnLennonFacts() {
-  const facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
-  ];
+  const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
-  var i = 0;
   var shoutFacts = [];
+  var i = 0;
 
-  while (i < facts.length) {
-    shoutFacts.push(facts[i] + "!!!");
+  while (i > 0) {
+    shoutFacts.push(facts + "!!!");
     i++;
   }
   return shoutFacts;
+
 }
